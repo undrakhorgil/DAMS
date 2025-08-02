89f848303ddad09c5886f1f4786ebeec75acc272
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2025-08-02 12:27:50 -0500

    3 little update testing

--- src/main/java/edu/miu/dams/DentalAppointmentManagementSystem.java
+++ src/main/java/edu/miu/dams/DentalAppointmentManagementSystem.java
@@ -3,15 +3,33 @@ package edu.miu.dams;
 import edu.miu.dams.service.AppointmentService;
 import edu.miu.dams.util.JSONConverterUtil;
 
+import java.util.Scanner;
+
 public class DentalAppointmentManagementSystem {
     public static void main(String[] args) {
         System.out.println("_______________________List info__________________________________");
         // Process data
-        var appointmentService = new AppointmentService();
 
-        String input = "1st Quarter of the year 2025";
+        try {
+            String input;
+            if (args.length > 0) {
+                // Use the first command-line argument as the input quarter
+                input = String.join(" ", args);
+            } else {
+                // Prompt user to enter quarter via console if no argument provided
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the Quarter you want to view appointments for (e.g., '1st Quarter of the year 2025'): ");
+                input = scanner.nextLine();
+            }
+
+            var appointmentService = new AppointmentService();
+
+
         var appointmentGivenQuater = appointmentService.getAppointmentGivenQuater(input);
         var appointments = JSONConverterUtil.convertPatientArrayToJSON(appointmentGivenQuater);
         System.out.println(appointments);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
